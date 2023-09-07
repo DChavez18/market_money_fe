bc2eaba5c31c6fce155803829c52b49cef1830b1
Derek Chavez <frrenose17@yahoo.com>
2023-09-07 15:15:09 -0600

    feat: create poro for market

---
+++ app/poros/market.rb
@@ -0,0 +1,23 @@
+class Market
+  attr_reader :id,
+              :name,
+              :street,
+              :city,
+              :county,
+              :state,
+              :lat,
+              :lon,
+              :vendor_count
+
+  def initialize(data)
+    @id = data[:attributes][:id]
+    @name = data[:attributes][:name]
+    @street = data[:attributes][:street]
+    @city = data[:attributes][:city]
+    @county = data[:attributes][:county]
+    @state = data[:attributes][:state]
+    @lat = data[:attributes][:lat]
+    @lon = data[:attributes][:lon]
+    @vendor_count = data[:attributes][:vendor_count]
+  end
+end
--- app/services/market_service.rb
+++ app/services/market_service.rb
@@ -2,4 +2,15 @@ class MarketService
   def all_markets
     get_url("api/v0/markets")
   end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "http://localhost:3000/") do |faraday|
+      faraday.headers = { 'CONTENT_TYPE': 'application/json' }
+    end
+  end
 end
